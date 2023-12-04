ee9058f69855be9da9d1be037ecbfc8293df4c65
gtbachel <gtbachel@ncsu.edu>
2023-12-04 18:29:07 -0500

    Permission command implemented

--- src/command/commands/permit_command.py
+++ src/command/commands/permit_command.py
@@ -9,9 +9,15 @@ class PermitCommand(abstract_command.AbstractCommand):
     args = [("user", argument_types.StringArgumentType, "Discord name of the user to update the permissions of."), ("role", argument_types.StringArgumentType, "None, Guest, Resident, Admin."), ("door", argument_types.StringArgumentType, "The name of the door to add the user to.")]
 
     def run(self):
-        #database access
-        response = "command ran with "
-        response += str(self.parsed_args["user"]) + " "
-        response += str(self.parsed_args["role"]) + " "
+        db_manger_instance = db_manager.DbManager()
+        if not db_manger_instance.checkLoggedIn(self.issuer_id):
+            raise SyntaxError("You are not logged in.")
+        try:
+            db_manger_instance.userPermissionUpdate(str(self.parsed_args["user"]), str(self.parsed_args["door"]), str(self.parsed_args["role"])
+        except:
+            raise SyntaxError("Could not grant permission.")
+        response = "Permission  "
+        response += str(self.parsed_args["role"]) + " given to @<"
+        response += str(self.parsed_args["user"]) + "> for "
         response += str(self.parsed_args["door"])
         return response
